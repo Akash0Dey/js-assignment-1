4eded47cc3625b4a29a5a446377634c2eff732f6
Akash <deymrakash99@gmail.com>
2024-12-18 22:50:12 +0530

    first prime above using recursion

--- 06_first_prime_above.js
+++ 06_first_prime_above.js
@@ -1,27 +1,42 @@
 // Do not rename a, use it as input for your program.
 // a will be a Whole number.
 // While testing we will change its value.
-const a = 0;
+const a = 5;
 // Print the first prime number above `a`
 // Printing more than one output or printing anything other than the first prime number above a might will be consider as error.
 // START YOUR CODE AFTER THIS LINE. DO NOT REMOVE THIS LINE
 
-let isPrime = false;
-let primeCandidate = a;
+const isNumberHasMoreThan2Factors = function (primeCandidate, divisor) {
+  if (Math.pow(divisor, 2) > primeCandidate) {
+    return true;
+  }
 
-while (isPrime === false) {
-    primeCandidate++;
-    isPrime = true;
-
-    for (let divisor = 2; divisor < primeCandidate; divisor++) {
-        if (primeCandidate % divisor === 0) {
-            isPrime = false;
-        }
-    }
-
-    if (primeCandidate < 2) {
-        isPrime = false;
-    }
+  return primeCandidate % divisor === 0 ? false : 
+  isNumberHasMoreThan2Factors(primeCandidate, divisor + 1);
 }
 
-console.log(primeCandidate);
+const isPrime = function (primeCandidate) {
+  if (primeCandidate < 2) {
+    return false;
+  }
+
+  return isNumberHasMoreThan2Factors(primeCandidate, 2);
+}
+
+const firstPrimeAbove = function (number) {
+  const primeCandidate = number + 1;
+
+  if (primeCandidate < 3) {
+    console.log(2);
+    return;
+  }
+
+  if (isPrime(primeCandidate)) {
+    console.log(primeCandidate);
+    return;
+  }
+
+  return firstPrimeAbove(primeCandidate);
+}
+
+firstPrimeAbove(a)
