1393cea5c14424b02e1b1fd99bd5173ab5dbe510
Akash <deymrakash99@gmail.com>
2024-12-18 21:43:29 +0530

    decimal to binary using recursion

--- 02_decimal_to_binary.js
+++ 02_decimal_to_binary.js
@@ -1,7 +1,7 @@
 // Do not rename a, use it as input for your program.
 // a will be a natural number.
 // While testing we will change its value.
-const a = 16;
+const a = 12;
 // Print the binary representation of a
 // If a = 12, then the output should be
 // 0
@@ -9,29 +9,17 @@ const a = 16;
 // 1
 // 1
 // START YOUR CODE AFTER THIS LINE. DO NOT REMOVE THIS LINE
-function range(start, end, step) {
-  let numbers = [];
 
-  for (let number = start; number < end; number += step) {
-    numbers.push(number);
+// recursion
+
+const decimalToBinary = function (number) {
+  if (number < 2) {
+    console.log(number);
+    return;
   }
 
-  return numbers;
+  console.log(number % 2);
+  return decimalToBinary(number >> 1);
 }
 
-const lastBit = function (number) {
-  console.log(number & 1); 
-  return number >> 1; 
-}
-
-range(0, Math.ceil(Math.log2(a + 1)), 1).reduce(lastBit, a);
-
-// let decimalNumber = a;
-// while (decimalNumber >= 0) {
-//   const binaryReminder = decimalNumber % 2;
-//   decimalNumber = (decimalNumber - binaryReminder) / 2;
-//   console.log(binaryReminder);
-//   if (decimalNumber === 0) {
-//       break;
-//   }
-// }
+decimalToBinary(a);
