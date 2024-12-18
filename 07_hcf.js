f138510c6c759f75800165f55f60e1d1cefb4965
Akash <deymrakash99@gmail.com>
2024-12-18 23:07:55 +0530

    hcf using recursion

--- 07_hcf.js
+++ 07_hcf.js
@@ -8,14 +8,27 @@ const b = 9;
 // Printing more than one output or printing anything other than HCF might will be consider as error.
 // START YOUR CODE AFTER THIS LINE. DO NOT REMOVE THIS LINE
 
-const max = (a > b) ? a : b;
-let hcf = undefined;
-
-for (let hcfCandidate = 1; hcfCandidate <= max; hcfCandidate++) {
-    if (a % hcfCandidate !== 0 ) {
-        continue;
-    }
-    hcf = ((b % hcfCandidate) === 0) ? hcfCandidate : hcf;
+const isDivisible = function (dividend, divisor) {
+  return dividend % divisor === 0;
 }
 
-console.log(hcf);
+const hcfPossible = function (num1, num2, hcfCandidate) {
+  if (isDivisible(num1, hcfCandidate) && isDivisible(num2, hcfCandidate)) {
+    return hcfCandidate;
+  }
+
+  return hcfPossible(num1, num2, hcfCandidate - 1);
+}
+
+const hcf = function (num1, num2) {
+  const max = Math.max(num1, num2);
+  const min = Math.min(num1, num2);
+
+  if (min === 0) {
+    return max;
+  }
+  
+  return hcfPossible(num1, num2, min);
+}
+
+console.log(hcf(a, b));
