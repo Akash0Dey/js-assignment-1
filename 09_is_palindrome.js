64855f994eef7b3617f7d5a9056bee8717363518
Akash <deymrakash99@gmail.com>
2024-12-19 10:20:53 +0530

    is Palindrome using recursion

--- 09_is_palindrome.js
+++ 09_is_palindrome.js
@@ -1,30 +1,28 @@
 // Do not rename a, use it as input for your program.
 // a will be a whole number.
 // While testing we will change its value.
-const a = 12321;
+const a = 121;
 // Print true if a is palindrome otherwise print false
 // Printing more than one output or printing anything other than palindrome or not palindrome might will be consider as error.
 // START YOUR CODE AFTER THIS LINE. DO NOT REMOVE THIS LINE
 
-// let remainingNumber = a;
-// let reverse = 0;
+const noOfDigit = function (number) {
+  if (number < 10) {
+    return 1;
+  }
 
-// while (remainingNumber != 0) {
-//     let reminder = remainingNumber % 10;
-//     reverse = reverse * 10 + reminder;
-//     remainingNumber = (remainingNumber - reminder) / 10;
-// }
-
-// const isPalindrome = (reverse === a);
-
-// console.log(isPalindrome);
-
-const numberString = "" + a;
-let reverseString = '';
-
-for (let index = 0; index >= (numberString.length - 1); index--) {
-    reverseString = numberString[index] + reverseString;
+  return 1 + noOfDigit(number / 10);
 }
 
-const isPalindrome = (reverseString === numberString);
-console.log(isPalindrome);
+const reverse = function (number) {
+  if (number < 10) {
+    return number;
+  }
+
+  const lastDigit = number % 10;
+  const positionValue = Math.pow(10, noOfDigit(number) - 1);
+
+  return lastDigit * positionValue + reverse(Math.floor(number / 10));
+}
+
+console.log(reverse(a) === a);
